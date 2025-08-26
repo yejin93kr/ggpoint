5265159e8856390cfd6c35c0d728b5a89ccaa12c
Koowin <56014482+Koowin@users.noreply.github.com>
2025-08-27 01:13:21 +0900

    Update script.py

--- script.py
+++ script.py
@@ -1,9 +1,13 @@
 import os
+import sys
+import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 LOGIN_URL = "https://www.library.kr/ggl/merge-user/login?target=ggpoint"
 CHALLENGE_URL = "https://www.library.kr/bookpoint/challenge/challenge"
